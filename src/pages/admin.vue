df8b2a973ac5c57e0dd42bb7c83332f449400cab
Panfile Kirill <kirillpanfile@gmail.com>
2025-02-07 21:19:34 +0200

    configure Nuxt to use 'src' directory and add admin page

---
+++ src/pages/admin.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+definePageMeta({
+  path: '/private/admin',
+  layout: false,
+})
+</script>
+
+<template>
+  <div>
+    Admin Page
+  </div>
+</template>
+
+<style>
+
+</style>
