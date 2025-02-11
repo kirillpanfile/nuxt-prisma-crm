74ea4d800f537437c4e8e22bb2eb15d105e546c3
Panfile Kirill <kirillpanfile@gmail.com>
2025-02-11 18:04:38 +0200

    Add Accept-Language header to HTTP requests for localization support

--- src/hooks/useHttpRequest.ts
+++ src/hooks/useHttpRequest.ts
@@ -2,9 +2,12 @@ type RequestParameters = Parameters<typeof $fetch>
 
 export default async function useHttpRequest<T = unknown>(path: RequestParameters[0], options: RequestParameters[1] = {}) {
   const token = useCookie('token')
+  const { $getLocale } = useNuxtApp()
+
   return await $fetch<T>(path, {
     ...options,
     headers: {
+      'Accept-Language': $getLocale(),
       ...(token.value && { Token: token.value as string }),
       ...options?.headers,
     },
