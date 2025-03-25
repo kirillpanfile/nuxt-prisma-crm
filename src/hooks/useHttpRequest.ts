427d0663ee37c86384e6d12e079a70d366a5e5c7
Panfile Kirill <kirillpanfile@gmail.com>
2025-03-25 19:53:27 +0200

    Update useHttpRequest.ts

--- src/hooks/useHttpRequest.ts
+++ src/hooks/useHttpRequest.ts
@@ -2,12 +2,12 @@ type RequestParameters = Parameters<typeof $fetch>
 
 export default async function useHttpRequest<T = unknown>(path: RequestParameters[0], options: RequestParameters[1] = {}) {
   const token = useCookie('token')
-  const { $getLocale } = useNuxtApp()
+  const { locale } = useI18n()
 
   return await $fetch<T>(path, {
     ...options,
     headers: {
-      'Accept-Language': $getLocale(),
+      'Accept-Language': locale.value,
       ...(token.value && { Token: token.value as string }),
       ...options?.headers,
     },
