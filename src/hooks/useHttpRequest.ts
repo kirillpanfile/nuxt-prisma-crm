520ba4347948806c6f123ceb9a37be18b4c6e3ed
Panfile Kirill <kirillpanfile@gmail.com>
2025-03-26 16:48:20 +0200

    Update Accept-Language header implementation in useHttpRequest to utilize Nuxt app's i18n locale

--- src/hooks/useHttpRequest.ts
+++ src/hooks/useHttpRequest.ts
@@ -2,12 +2,11 @@ type RequestParameters = Parameters<typeof $fetch>
 
 export default async function useHttpRequest<T = unknown>(path: RequestParameters[0], options: RequestParameters[1] = {}) {
   const token = useCookie('token')
-  const { locale } = useI18n()
-
-  return await $fetch<T>(path, {
+  const { $i18n } = useNuxtApp()
+  return $fetch<T>(path, {
     ...options,
     headers: {
-      'Accept-Language': locale.value,
+      'Accept-Language': $i18n.locale.value,
       ...(token.value && { Token: token.value as string }),
       ...options?.headers,
     },
