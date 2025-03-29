60d44ec47cf1a159b19b90bf1ed476a7e0cdb0f1
Panfile Kirill <kirillpanfile@gmail.com>
2025-03-29 19:48:19 +0200

    Implement singleton pattern for PrismaClient to ensure a single instance is used throughout the application, improving resource management.

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,17 +1,13 @@
-import process from 'node:process'
 import { PrismaClient } from '@prisma/client'
 
+let prisma: PrismaClient
+
 function prismaClientSingleton() {
-  return new PrismaClient()
+  console.log('prismaClientSingleton')
+  if (!prisma) {
+    prisma = new PrismaClient()
+  }
+  return prisma
 }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production')
-  globalThis.prismaGlobal = prisma
+export default prismaClientSingleton()
