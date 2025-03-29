22a5e544fcc2057db876d41be8f15ae0881c258b
Panfile Kirill <kirillpanfile@gmail.com>
2025-03-29 16:06:56 +0200

    Enhance useColorMode hook to set cookie expiration to 7 days for improved user experience

--- src/hooks/useColorMode.ts
+++ src/hooks/useColorMode.ts
@@ -5,6 +5,7 @@ let stopWatch: WatchStopHandle | null = null
 export default function useColorMode() {
   const colorMode = useCookie('colorMode', {
     default: () => 'auto',
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
   })
 
   const syncColorMode = () => useHead({ htmlAttrs: { 'data-mode': colorMode.value } })
